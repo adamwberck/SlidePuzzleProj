bbcfd8d3e50a83f62cd3ff73870df626a06774ea
Adam <adamwberck@gmail.com>
2019-10-26 18:31:38 -0400

    Added a menu activity. And a lab singleton.

---
+++ app/src/main/java/com/example/slidepuzzleproj/MenuActivity.java
@@ -0,0 +1,42 @@
+package com.example.slidepuzzleproj;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MenuActivity extends Activity {
+    private View changeImageButton;
+    private ImageView puzzleImageView;
+    private Button playButton;
+    private PuzzleBoard currentBoard;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu); //attach the layout
+        //currentBoard = PuzzleLab.get(this).getCurrentBoard();
+        changeImageButton = findViewById(R.id.change_image);
+        puzzleImageView = findViewById(R.id.puzzle_image);
+
+        //puzzleImageView.setImageBitmap(currentBoard.getPuzzleImage());
+
+        playButton = findViewById(R.id.play_button);
+        changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO add image change activity
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO goto puzzle activity
+            }
+        });
+    }
+
+}
