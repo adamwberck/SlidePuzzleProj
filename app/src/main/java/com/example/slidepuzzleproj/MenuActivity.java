2502e6be0c233b632769b3cdce65d468e5f1e05e
Mike Lauw <tug01761@temple.edu>
2019-10-27 22:12:20 -0400

    changing image through button(accessing emulator's library)

--- app/src/main/java/com/example/slidepuzzleproj/MenuActivity.java
+++ app/src/main/java/com/example/slidepuzzleproj/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.slidepuzzleproj;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +13,14 @@ import android.widget.ImageView;
 public class MenuActivity extends Activity {
     private View changeImageButton;
     private ImageView puzzleImageView;
-    private Button playButton, dimenButton;
+    private Button playButton, dimenButton, changeButton;
     private int width, height;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu); //attach the layout
@@ -27,14 +32,17 @@ public class MenuActivity extends Activity {
         //puzzleImageView.setImageBitmap(currentBoard.getPuzzleImage());
 
 
-        changeImageButton = findViewById(R.id.change_image);
-        changeImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO add image change activity
 
+        changeButton = (Button)findViewById(R.id.test);
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                openGallery();
             }
         });
+
+
 
         dimenButton = findViewById(R.id.dimension_button);
         String[] dimen = dimenButton.getText().toString().split("x");
@@ -61,8 +69,23 @@ public class MenuActivity extends Activity {
             }
         });
 
+    }
 
+    private void openGallery()
+    {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK  && requestCode == PICK_IMAGE)
+        {
+            imageUri = data.getData();
+            puzzleImageView.setImageURI(imageUri);
+        }
     }
 
 }
